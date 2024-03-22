b6a2558b1fffee16e4efe8cd1ee9629d7e5943c0
rabindra12345 <rabindradahal2015@gmail.com>
2024-03-22 18:34:55 +0545

    first commit

---
+++ src/test/java/com/rabindra/springmvcthymeleaf/SpringmvcThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabindra.springmvcthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
