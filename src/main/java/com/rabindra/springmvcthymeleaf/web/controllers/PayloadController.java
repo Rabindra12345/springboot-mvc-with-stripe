d6521c1b7f993c4d4234687826c7d505a620e669
rabindra12345 <rabindradahal2015@gmail.com>
2024-04-15 16:56:08 +0545

    added payload page

--- src/main/java/com/rabindra/springmvcthymeleaf/web/controllers/PayloadController.java
+++ src/main/java/com/rabindra/springmvcthymeleaf/web/controllers/PayloadController.java
@@ -4,13 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class LoginController {
+public class PayloadController {
 
+    private static final String PAYLOAD_URL = "payload/payload";
 
-    private static final String LOGIN_VIEW_PAGE = "user/login";
-
-    @RequestMapping("/login")
-    public String login(){
-        return LOGIN_VIEW_PAGE;
+    @RequestMapping("/payload")
+    public String getPayloadUrl(){
+        return PAYLOAD_URL;
     }
 }
